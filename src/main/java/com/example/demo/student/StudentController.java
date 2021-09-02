36d2723157aa9e1852e921bf459e39fdc63e2c85
Vee Ko <veeko2021@gmail.com>
2021-09-02 11:42:02 +0300

    All fixed and working

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -38,7 +38,7 @@ public class StudentController {
         studentService.deleteStudent(studentId);
     }
 
-    @PutMapping(path = {"studentId"})
+    @PutMapping(path = "{studentId}")
     public void updateStudent(
             @PathVariable("studentId") Long studentId,
             @RequestParam(required = false) String name,
